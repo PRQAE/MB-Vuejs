e8b88e5235fb5830049087a78e5858f5588e6ccd
junaidjahan <70884217+junaidjahan@users.noreply.github.com>
2023-09-15 12:31:41 +0500

    feat: base components

---
+++ src/@core/components/base/BaseMenu.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <VMenu>
+    <template v-for="(_, name) in $slots" #[name]="slotProps">
+      <slot :name="name" v-bind="slotProps || {}" />
+    </template>
+  </VMenu>
+</template>
